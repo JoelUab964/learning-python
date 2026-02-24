348a9294738d2f00ee3ca0535ba222fe74a48365
joel <joel.lopez@uab.edu.bo>
2026-02-23 22:34:21 -0400

    variables

---
+++ 02-Variables en Python/asignacion_variables.py
@@ -0,0 +1,23 @@
+primer_numero=10
+segundo_numero=10
+#podemos simplificar la asignacion de arriba 
+#la siguiente manera :
+primer_numero=segundo_numero=10
+segundo_numero = 5
+
+print(primer_numero)
+print(segundo_numero)
+
+primer_nombre="Juan"
+segundo_nombre="Maria"
+
+primer_nombre,segundo_nombre="Juan", "Maria"
+print(segundo_nombre)
+print(primer_nombre)
+
+a,b,c,d=50,100,135,200
+
+print(a)
+print(b)
+print(c)
+print(d)
---
+++ 02-Variables en Python/intro-variables.py
@@ -0,0 +1,12 @@
+nombre="Joel"
+apellido="Lopez"
+edad=26
+hombre=True 
+lenguaje_faborito="Espanol" 
+mi_comida_favorita="pizza"
+
+print(nombre)
+print(apellido)
+print(edad)
+print(lenguaje_faborito)
+print (hombre)
